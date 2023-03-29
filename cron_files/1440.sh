5cbf8d8e5d67ba2d4b82997f9253ef9aa44a0375
dkmur <dkmur74@gmail.com>
2023-03-29 16:05:24 +0200

    add golbat backup

--- cron_files/1440.sh
+++ cron_files/1440.sh
@@ -19,3 +19,25 @@ then
   diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
   echo "[$start] [$stop] [$diff] rpl1440 worker stats processing" >> $folder/logs/log_$(date '+%Y%m').log
 fi
+
+## backup golbat db
+if "$golbat_backup"
+then
+  start=$(date '+%Y%m%d %H:%M:%S')
+  mkdir -p $folder/golbatbackup
+  MYSQL_PWD=$sqlpass mysqldump -u$sqluser -h$dbip -P$dbport --no-data --routines $scannerdb > $folder/golbatbackup/golbatbackup_$(date +%Y-%m-%d).sql
+  MYSQL_PWD=$sqlpass mysqldump -u$sqluser -h$dbip -P$dbport $scannerdb gym pokestop spawnpoint schema_migrations >> $folder/golbatbackup/golbatbackup_$(date +%Y-%m-%d).sql
+  cd $folder/golbatbackup && tar --remove-files -czvf golbatbackup_$(date +%Y-%m-%d).sql.tar.gz golbatbackup_$(date +%Y-%m-%d).sql
+  stop=$(date '+%Y%m%d %H:%M:%S')
+  diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
+  echo "[$start] [$stop] [$diff] daily golbat backup" >> $folder/logs/log_$(date '+%Y%m').log
+fi
+## golbat db backup cleanup
+if "$golbat_backup"
+then
+  start=$(date '+%Y%m%d %H:%M:%S')
+  find $folder/golbatbackup -type f -mtime +$golbat_backup_days -exec rm -f {} \;
+  stop=$(date '+%Y%m%d %H:%M:%S')
+  diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
+  echo "[$start] [$stop] [$diff] daily golbat backup cleanup" >> $folder/logs/log_$(date '+%Y%m').log
+fi
