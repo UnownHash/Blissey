00d0f6475078ac4688714d36bdd6cb632da59af9
dkmur <dkmur74@gmail.com>
2023-04-06 14:50:11 +0200

    change rpl15 quest processing

--- cron_files/15.sh
+++ cron_files/15.sh
@@ -34,7 +34,7 @@ fi
 if "$questareastats"
 then
   start=$(date '+%Y%m%d %H:%M:%S')
-  MYSQL_PWD=$sqlpass mysql -u$sqluser -h$dbip -P$dbport $blisseydb -NB -e "call rpl15questarea();"
+  MYSQL_PWD=$sqlpass mysql -u$sqluser -h$dbip -P$dbport $blisseydb -NB -e "SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED; call rpl15questarea();"
   stop=$(date '+%Y%m%d %H:%M:%S')
   diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
   echo "[$start] [$stop] [$diff] rpl15 quest area stats processing" >> $folder/logs/log_$(date '+%Y%m').log
