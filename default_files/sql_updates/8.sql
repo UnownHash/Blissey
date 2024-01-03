e0865ed4f35427da1f6e413b6a5553659ce747c7
dkmur <dkmur74@gmail.com>
2024-01-03 13:58:08 +0100

    add auth time

--- default_files/sql_updates/8.sql
+++ default_files/sql_updates/8.sql
@@ -1,5 +1,8 @@
 ALTER TABLE  `dragoLog`
-  add column `noAccount` int default NULL after `backoff`;
+  add column `noAccount` int default NULL after `backoff`,
+  add column `minAuthT` int default NULL,
+  add column `maxAuthT` int default NULL,
+  add column `avgAuthT` int default NULL;
 
 -- update db version
 UPDATE version set version = 8 where version.key = 'blissey';
