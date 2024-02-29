520cc0e53701088c14b1f913abf35db3ee2bcf93
dkmur <dkmur74@gmail.com>
2024-02-29 16:42:29 +0100

    fix sql migration, dont overwrite previous versions idiot

--- default_files/sql_updates/10.sql
+++ default_files/sql_updates/10.sql
@@ -1,10 +1,20 @@
 ALTER TABLE  `dragoLog`
-  rename column `mitmUnknown` to `mitm500`,
-  rename column `mitmNoGame` to `mitm501`,
-  rename column `mitmLogginIn` to `mitm502`,
-  rename column `mitmTokenRej` to `mitm503`,
-  drop column `mitmNotLogged`,
-  add column `monchange` int default NULL;
+  add column `swTotal` int default NULL AFTER `login`,
+  add column `swStop` int default NULL AFTER `swTime`,
+  add column `swLLapi` int default NULL AFTER `swStop`,
+  add column `swQdist` int default NULL AFTER `swLLapi`;
+
+ALTER TABLE  `dragoLog_fort`
+  add column `swTotal` int default NULL AFTER `gmo8`,
+  add column `swStop` int default NULL AFTER `swTime`,
+  add column `swLLapi` int default NULL AFTER `swStop`,
+  add column `swQdist` int default NULL AFTER `swLLapi`;
+
+ALTER TABLE  `dragoLog_invasion`
+  add column `swTotal` int default NULL AFTER `gmo8`,
+  add column `swStop` int default NULL AFTER `swTime`,
+  add column `swLLapi` int default NULL AFTER `swStop`,
+  add column `swQdist` int default NULL AFTER `swLLapi`;
 
 -- update db version
 UPDATE version set version = 10 where version.key = 'blissey';
---
+++ default_files/sql_updates/11.sql
@@ -0,0 +1,10 @@
+ALTER TABLE  `dragoLog`
+  rename column `mitmUnknown` to `mitm500`,
+  rename column `mitmNoGame` to `mitm501`,
+  rename column `mitmLogginIn` to `mitm502`,
+  rename column `mitmTokenRej` to `mitm503`,
+  drop column `mitmNotLogged`,
+  add column `monchange` int default NULL;
+
+-- update db version
+UPDATE version set version = 10 where version.key = 'blissey';
